f0be7cc3bc028a396da6a83f033298dc193755bf
lucsalesjr <lucianosales_jr@hotmail.com>
2021-01-30 21:09:55 -0300

    inclusão da contagem de erros

--- a.py
+++
@@ -1,8 +0,0 @@
-frutas = ['Banana', 'Morango', 'Maçã', 'Uva']
-
-fruta_buscada = 'Melancia'
-if fruta_buscada in frutas:
-    print(frutas.index(fruta_buscada))
-else:
-    print('Desculpe, a {} não está na lista frutas'.format( fruta_buscada))
-
--- teste.py
+++
@@ -1,4 +0,0 @@
-numero_random = random.random() * 100
-numero_random
-int(numero_random)
-round(numero_random)
